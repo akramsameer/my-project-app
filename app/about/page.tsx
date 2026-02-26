95977ec8e9f4d42d2464a501282fbf47206c2bc5
Akram Samir <akram.sameer99@gmail.com>
2026-02-26 17:28:04 +0200

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components";
+
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is the open-source edition.</p>
+      <Button variant="secondary">Learn More</Button>
+    </main>
+  );
+}
